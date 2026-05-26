835350028371c388de15f842acf47840386abfdb
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2026-05-26 16:48:56 +0200

    feat(mq-fretboard): add component

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { MqFretboard } from "./mq-fretboard/mq-fretboard.js";
 export { MqPiano } from "./mq-piano/mq-piano.js";
 export { MqPianoChord } from "./mq-piano-chord/mq-piano-chord.js";
 export { MqPianoScale } from "./mq-piano-scale/mq-piano-scale.js";
